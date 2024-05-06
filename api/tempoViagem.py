38fe3f970c83c4c74542f372777924739b25e220
Arthur <arthurmielemalveste@gmail.com>
2024-05-06 08:06:24 -0300

    algoritimo para calcular o tempo

---
+++ api/tempoViagem.py
@@ -0,0 +1,8 @@
+
+distancia = float(input("Digite a distância da viagem (em km): "))
+
+velocidade = float(input("Digite a velocidade média da viagem (em km/h): "))
+
+tempo = distancia / velocidade
+
+print(f"O tempo de viagem será de aproximadamente {tempo:.2f} horas.")
